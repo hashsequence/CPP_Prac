10fadf801a0346588fc8896456bfa19685af5b94
Avery Wong <avwong13@g.ucla.edu>
2019-03-19 02:56:20 -0700

    Update serializeAndDeserializeBST.cpp

--- cppAlgos/serializeAndDeserializeBST.cpp
+++ cppAlgos/serializeAndDeserializeBST.cpp
@@ -29,13 +29,13 @@ public:
             if (front != nullptr) {
                 s += to_string(front->val) + ",";
                 
-               if (front->left == nullptr) {
+               if (front->left != nullptr) {
                  q.push(front->left);
                } else {
                  q.push(nullptr);
                }
                 
-               if (front->right == nullptr) {
+               if (front->right != nullptr) {
                 q.push(front->right);
                } else {
                 q.push(nullptr);
@@ -44,11 +44,11 @@ public:
             } else {
                 s += "null,";
             }
-            //cout << s << endl;
+          //  cout << s << endl;
         }
-        s = *(s.end()-1) + ']';
-       // cout << s << endl;
-        return "[1,2,3,null,null,4,5]";
+        *(s.end()-1) = ']';
+       cout << s << endl;
+       // return "[1,2,3,null,null,4,56]";
         return s;
     }
 
@@ -58,7 +58,8 @@ public:
             return nullptr;
         }
         data = data.substr(1,data.size()-2);
-        vector<TreeNode*> q;
+        queue<TreeNode*> q;
+        queue<TreeNode*> levelOrder;
         int start = 0;
         int end = 0;
         int i = 0;
@@ -66,18 +67,32 @@ public:
             if (data[i] == ',' || i == data.size()) {
                 end = i;
                 string token = data.substr(start, end-start);
-                cout << token << endl;
-                q.push(new TreeNode(token));
+                //cout << token << endl;
+                q.push([](string val){return val == "null" ? nullptr : new TreeNode(atoi(val.c_str()));}(token));
                 start = end + 1;
             } 
         }
-        TreeNode* root;
-        
+        TreeNode* root = q.front();
+        q.pop();
+        levelOrder.push(root);
+        while (!q.empty()) {
+            TreeNode* treePtr = levelOrder.front();
+            levelOrder.pop();
+            TreeNode* left = q.front();
+            q.pop();
+            TreeNode* right = q.front();
+            q.pop();
+            
+            if (left != nullptr) {
+                treePtr->left = left;
+                levelOrder.push(left);
+            } 
+            if (right != nullptr) {
+                treePtr->right = right;
+                levelOrder.push(right);
+            }
+        }
         return root;
-        
-        
-        
-        return nullptr;
     }
     
   
